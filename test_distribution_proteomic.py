a10d023f8f119b2a19d10e9a617060b6e2a71498
Jen <keungjenna@gmail.com>
2023-07-08 20:15:37 +0100

    test distribution

--- test_distribution_proteomic.py
+++ test_distribution_proteomic.py
@@ -9,20 +9,20 @@ import pickle
 df = pd.read_csv('proteomics/Data/initial_tstats.csv', index_col=0)
 
 
-for i in range(8):
-    index = random.randrange(0, len(df)) #Gives index from 0 to (len(df)-1), better for indexing with unpickled files which are stored as lists
-    pathway_list = []
 
-    for i in range(1,11): #go through each of the 10 folders that have 10k in
-        path = os.getcwd() + '/proteomics/Results' + str(index)
-       
-        for filename in os.listdir(path):
-            with open(os.path.join(path, filename), 'rb') as f: # open in readonly mode
-                list1 = pickle.load(f)
-            pathway_list.append(list1[index])
+index = random.randrange(0, len(df)) #Gives index from 0 to (len(df)-1), better for indexing with unpickled files which are stored as lists
+pathway_list = []
 
-    with open('proteomics/Data/test_distribution'+str(index)+'.txt', 'w') as file:
-        file.write(','.join(str(i) for i in pathway_list))
+for i in range(1,11): #go through each of the 10 folders that have 10k in
+    path = os.getcwd() + '/proteomics/Results' + str(i)
+    
+    for filename in os.listdir(path):
+        with open(os.path.join(path, filename), 'rb') as f: # open in readonly mode
+            list1 = pickle.load(f)
+        pathway_list.append(list1[index])
+
+with open('proteomics/Data/test_distribution'+str(index)+'.txt', 'w') as file:
+    file.write(','.join(str(i) for i in pathway_list))
 
 
 
